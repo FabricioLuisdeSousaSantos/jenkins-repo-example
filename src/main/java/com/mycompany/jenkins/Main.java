19342d81ff5a274eef38868c35117172fa6861d3
Fabrício Luís <90388770+FabricioLuisdeSousaSantos@users.noreply.github.com>
2023-08-06 14:09:36 -0300

    Update Main.java

--- src/main/java/com/mycompany/jenkins/Main.java
+++ src/main/java/com/mycompany/jenkins/Main.java
@@ -2,7 +2,7 @@ package com.mycompany.jenkins;
 
 public class Main {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World 04!");
     }
 }
